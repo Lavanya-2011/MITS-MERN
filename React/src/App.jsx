87318cd6fe4f8503cfe86560af4ee2a57b3cd36e
Lavanya <lavsd371@gmail.com>
2026-01-29 11:34:48 +0530

    Thursday

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,40 +1,54 @@
-import { Route, Routes } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
-import Service from "./pages/Service";
-import Contact from "./pages/Contact";
 import About from "./pages/About";
-import Counter from "./component/Counter";
-import Login from "./Auth/Login";
-import SignUp from "./Auth/signup";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
 
+import { Route, Routes } from "react-router-dom";
+
+import UseState from "./pages/Hooks/UseState";
 import Hook from "./Auth/Hooks/Hook";
-import Usestate from "./Auth/Hooks/Usestate";
-import Useeffect from "./Auth/Hooks/Useeffect";
+
+import Login from "./Auth/Login";
+import Signup from "./Auth/signup";
+
+import UseEffect from "./Auth/Hooks/Useeffect";
 import UseEffectApi from "./Auth/Hooks/UseEffectApi";
 import UseRef from "./Auth/UseRef";
 import UseReducer from "./pages/Hooks/UseReducer";
+import UseMemo from "./Auth/Hooks/UseMemo";
+import UseCallback from "./Auth/Hooks/UseCallback";
+
 const App = () => {
   return (
     <>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Home name="Vamsi" />} />
-        <Route path="/about" element={<About name="Vamsi" age={23} dept="ECE" />} />
+        <Route path="/" element={<Home name="Namrutha" />} />
+
+        <Route
+          path="/about"
+          element={<About name="Namrutha" age={20} dept="CS&DS" />}
+        />
+
         <Route path="/contact" element={<Contact />} />
         <Route path="/service" element={<Service />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
 
-        {/*  Hooks */}
+        <Route path="/useState" element={<UseState />} />
+
+        {/* 🔽 Hooks Parent Route */}
         <Route path="/hook" element={<Hook />}>
-          <Route path="usestate" element={<Usestate />} />
-          <Route path="useeffect" element={<Useeffect />} />
-          <Route path="useeffectapi" element={<UseEffectApi />} />
-          <Route path="useref" element={<UseRef />} />
-          <Route path="usereducer"element={<UseReducer/>}/>
+          <Route path="useEffect" element={<UseEffect />} />
+          <Route path="useEffectApi" element={<UseEffectApi />} />
+          <Route path="useRef" element={<UseRef />} />
+          <Route path="useReducer" element={<UseReducer />} />
+          <Route path="useMemo" element={<UseMemo />} />
+          <Route path="usecallback" element={<UseCallback />} />
         </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
--- React/src/Auth/Hooks/Hook.jsx
+++ React/src/Auth/Hooks/Hook.jsx
@@ -10,6 +10,8 @@ const Hook = () => {
         <li><Link to="useeffectapi">UseEffectWithApi</Link></li>
         <li><Link to="useref">useRef</Link></li>
         <li><Link to="usereducer">UseReducer</Link></li>
+        <li><Link to="useMemo">UseMemo</Link></li>
+        <li><Link to="usecallback">UseCallback</Link></li>
       </ol>
       <Outlet />
     </div>
