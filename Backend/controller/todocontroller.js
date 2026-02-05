e2c5c0fcec6719a9866c18375ca5c4f6d276cb2d
Lavanya <lavsd371@gmail.com>
2026-02-05 10:33:24 +0530

    Thursday-1

--- Backend/controller/todocontroller.js
+++ Backend/controller/todocontroller.js
@@ -1,31 +1,75 @@
 let todos=[];
 let id=1;
-exports.getTodo=(req,res)=>{
-    res.status(200).json(todos)
+const Todo = require("../models/todo");
+//exports.getTodo = (req,res)=>{
+    {/*res.send("Get working")*/}
+   // res.status(200).json(todos)
+//};
+exports.getTodo = async(req,res)=>{
+    try{
+        const todos= await Todo.find();
+        res.status(200).json(todos);
+
+    }catch(err){
+        res.status(500).send(err);
+    }
 };
 
-exports.createTodo=(req,res)=>{
-     const {task}=req.body;
-     const newTodo={
+exports.createTodo=async(req,res)=>{
+    {/*res.send("Post working")*/}
+    try{
+        const {task}=req.body;
+        if(task===undefined)
+            return res.status(401).json({message:"Task not founfd"})
+        const todos= await Todo.create({
+            task,
+            completed:false
+        })
+        res.status(201).json(todos);
+    
+    }catch(err){
+        res.status(500).send(err);
+    }
+
+   {/*} const newTodo = {
         id:id++,
         task,
-        completed:false
+        Completed:false
+    //}
+    todos.push(newTodo)
+    res.json(newTodo)*/}
 
-     }
-     todos.push(newTodo)
-     res.json(newTodo)
 };
 
-exports.updateTodo=(req,res)=>{
-    const todo=todos.find((t)=>t.id===parseInt(req.params.id));
-    if(!todo){
-        res.json({message:"Todo not found"})
-    }
+exports.updateTodo=async (req,res)=>{
+    try{
+        const todo=await Todo.findById(req.params.id);
+        if(!todo) {
+            res.json({message:"Todo not found"})
+}
     todo.task=req.body.task || todo.task;
-    todo.completed=req.body.completed===undefined?todo.completed:req.body.completed
-    res.json(todo)
+    todo.completed = req.body.completed===undefined?todo.completed:req.body.completed
+    await todo.save();
+    res.status(200).json(todo)
+} catch (err){
+    res.status(500).send(err);
+}
+
 };
 
-exports.deleteTodo=(req,res)=>{
-     res.send("Delete working")
+exports.deleteTodo=async(req,res)=>{
+    try{
+        await Todo.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:"Task deleted Successfully"})
+
+    }catch(err){
+        res.status(500).send(err);
+    }
+    
 };
+
+
+//api/todo/  ->in the postman
+//api/todo/create
+//api/todo/update
+//api/todo/delete/1
